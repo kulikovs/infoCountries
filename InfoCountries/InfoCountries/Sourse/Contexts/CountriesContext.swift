7ddbf8ce8ab8180bd43c8dedac642cd74b1ac55a
Sergey Kulikov <sergey.kulikov@Artem-Chabanniy-2.local>
2016-11-25 15:09:54 +0200

    ObjectMapping….

--- InfoCountries/InfoCountries/Sourse/Contexts/CountriesContext.swift
+++ InfoCountries/InfoCountries/Sourse/Contexts/CountriesContext.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Foundation
+import Alamofire
+import ObjectMapper
 
 let httpMethodGet = "GET"
 let countriesURLString = "http://api.worldbank.org/country?per_page=10&format=json&page=1"
@@ -21,21 +23,36 @@ class CountriesContext: NSObject {
     }
     
     func prepareToLoad() {
-        let request = NSMutableURLRequest.init(url: NSURL(string: countriesURLString)! as URL)
-        let session = URLSession.shared
-        self.dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-            do {
-                if (error != nil) {
-                } else {
-                    let jsonData = try JSONSerialization.jsonObject(with: data!, options: []) as! NSArray
-                    self.parseResult(result: jsonData)
-                }
-            }
-            catch {
-                
-            }
+//        Alamofire.request(countriesURLString).responseJSON { response in
+//            if let json: NSArray = response.result.value as! NSArray? {
+//                print("JSON: \(json)")
+//                var model = TestModel(JSONString: "")
+//                
+//            }
+//        }
+      
+        Alamofire.request(countriesURLString).responseData(completionHandler: { data in
+            
+            
         })
+
+     //   let request = NSMutableURLRequest.init(url: NSURL(string: countriesURLString)! as URL)
+//        let session = URLSession.shared
+//        self.dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+//            do {
+//                if (error != nil) {
+//                } else {
+//                    let jsonData = try JSONSerialization.jsonObject(with: data!, options: []) as! NSArray
+//                    self.parseResult(result: jsonData)
+//                }
+//            }
+//            catch {
+//                
+//            }
+//        })
+//        
+//        self.dataTask?.resume()
         
-        self.dataTask?.resume()
+    
     }
 }
---
+++ InfoCountries/InfoCountries/Sourse/Models/TestModel/TestModel.swift
@@ -0,0 +1,30 @@
+//
+//  TestModel.swift
+//  InfoCountries
+//
+//  Created by Sergey Kulikov on 11/25/16.
+//  Copyright © 2016 Sergey Kulikov. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class TestModel: Mappable {
+    var name:         NSString?
+    var capital:      NSString?
+    var population:   Int32?
+    var callingCode:  Int?
+    var numericCode:  Int?
+    
+    required init?(map: Map){
+        
+    }
+    
+    func mapping(map: Map) {
+        self.name   <- map["name"]
+        capital     <- map["capital"]
+        population  <- map["population"]
+        callingCode <- map["callingCode"]
+        numericCode <- map["numericCode"]
+    }
+}
